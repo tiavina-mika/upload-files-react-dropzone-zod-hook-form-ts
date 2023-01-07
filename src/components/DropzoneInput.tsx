016f513d4e9281bfda119642b08f559a76ddc5cd
Tiavina Michael Ralainirina <ralainirinatiavina@gmail.com>
2023-01-07 17:47:46 +0300

    display image preview

--- src/components/DropzoneInput.tsx
+++ src/components/DropzoneInput.tsx
@@ -1,4 +1,4 @@
-import { FC, useCallback } from "react";
+import { FC, useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
 
 type Props = {
@@ -7,23 +7,49 @@ type Props = {
 };
 
 const DropzoneInput: FC<Props> = ({ onChange, onBlur, ...rest }) => {
+  const [files, setFiles] = useState<FileList[] | Blob[] | MediaSource[]>([]);
+
   const onDrop = useCallback(
     (acceptedFiles) => {
       // Do something with the files
       console.log({ acceptedFiles });
-      onChange(acceptedFiles);
+      const filesWithPreview = acceptedFiles.map((file, index) => {
+        return Object.assign(file, {
+          preview: URL.createObjectURL(file),
+          id: index
+        });
+      });
+      const allFiles = [...files, ...filesWithPreview];
+      onChange(allFiles);
+      setFiles(allFiles);
     },
-    [onChange]
+    [onChange, files]
   );
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
   return (
-    <div {...getRootProps()}>
-      <input {...getInputProps({ onChange, onBlur })} />
-      {isDragActive ? (
-        <p>Drop the files here ...</p>
-      ) : (
-        <p>Drag 'n' drop some files here, or click to select files</p>
-      )}
+    <div>
+      <div {...getRootProps()}>
+        <input {...getInputProps({ onChange, onBlur })} />
+        {isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        )}
+      </div>
+
+      {files.map((file, index) => (
+        <li key={index}>
+          <img
+            alt=""
+            src={file.preview}
+            // Revoke data uri after image is loaded
+            onLoad={() => {
+              URL.revokeObjectURL(file.preview);
+            }}
+          />
+          {/* <button onClick={removeFile(file)}>Remove File</button> */}
+        </li>
+      ))}
     </div>
   );
 };
