f0a5896b2b5011ff1b474b5e1a5251b3be18ff95
Tiavina Michael Ralainirina <ralainirinatiavina@gmail.com>
2023-01-09 18:26:23 +0300

    comment code

--- src/utils/validations/uploadValidations.ts
+++ src/utils/validations/uploadValidations.ts
@@ -10,32 +10,42 @@ const MAX_IMAGE_UPLOAD = 1;
 export const uploadSchema = z.object({
   image: z
     .any()
+    // file count validation
     .refine((files: File[]) => {
       return files.length === 0 || files.length === MAX_IMAGE_UPLOAD;
     }, "Upload only one image")
+    // file size validation
     .refine((files) => {
       return !hasFilesMaxSize(files, MAX_IMAGE_SIZE);
     }, "Max file required is " + MAX_IMAGE_SIZE + "MB")
+    // file types validation
     .refine((files: File[]): boolean => {
       if (!files.length) return true;
       return hasAcceptedFileTypes(files, ACCEPTED_IMAGE_TYPES);
     }, ACCEPTED_IMAGE_TYPES.map((type: string) => "." + type) + " are accepted")
+    // get only one file from list
     .transform((files: File[]) => {
+      if (!files || files.length === 0) return;
       return files[0];
     }),
   csv: z
     .any()
+    // file count validation
     .refine((files: File[]) => {
       return files.length === 0 || files.length === MAX_IMAGE_UPLOAD;
     }, "Upload only one csv file")
+    // file size validation
     .refine((files) => {
       return !hasFilesMaxSize(files, MAX_IMAGE_SIZE);
     }, "Max file required is " + MAX_IMAGE_SIZE + "MB")
+    // file types validation
     .refine((files: File[]): boolean => {
       if (!files.length) return true;
       return hasAcceptedFileTypes(files, ACCEPTED_CSV_TYPES);
     }, ACCEPTED_CSV_TYPES.map((type: string) => "." + type) + " are accepted")
+    // get only one file from list
     .transform((files: File[]) => {
+      if (!files || files.length === 0) return;
       return files[0];
     })
 });
